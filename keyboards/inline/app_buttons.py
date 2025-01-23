4d733d56f863512d7b459f8aca68b0fc0707baf3
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-23 17:15:31 +0300

    Fix links to application Hiddify

--- keyboards/inline/app_buttons.py
+++ keyboards/inline/app_buttons.py
@@ -7,15 +7,15 @@ def get_apps_murkup():
 
     actions.add(InlineKeyboardButton(text="Windows", url="https://github.com/hiddify/hiddify-next/releases/"
                                                          "download/v2.0.5/Hiddify-Windows-Setup-x64.exe"),
-                InlineKeyboardButton(text="MacOS", url="https://github.com/hiddify/hiddify-next/"
-                                                       "releases/download/v2.0.5/Hiddify-MacOS.dmg"),
+                InlineKeyboardButton(text="MacOS", url="https://apps.apple.com/ru/app/"
+                                                       "hiddify-proxy-vpn/id6596777532"),
                 InlineKeyboardButton(text="Linux", url="https://github.com/hiddify/hiddify-next/releases/"
                                                        "download/v2.0.5/Hiddify-Debian-x64.deb")
                 )
-    actions.add(InlineKeyboardButton(text="iOS", url="https://github.com/hiddify/hiddify-next/"
-                                                     "releases/download/v2.0.5/Hiddify-iOS.ipa"),
-                InlineKeyboardButton(text="Android", url="https://github.com/hiddify/hiddify-next/releases/"
-                                                         "download/v2.0.5/Hiddify-Android-universal.apk")
+    actions.add(InlineKeyboardButton(text="iOS", url="https://apps.apple.com/ru/app/"
+                                                     "hiddify-proxy-vpn/id6596777532"),
+                InlineKeyboardButton(text="Android", url="https://play.google.com/store/"
+                                                         "apps/details?id=app.hiddify.com")
                 )
     actions.add(InlineKeyboardButton(text="Выбрать свой вариант", url="https://github.com/hiddify/"
                                                                       "hiddify-app/releases/tag/v2.0.5"))
