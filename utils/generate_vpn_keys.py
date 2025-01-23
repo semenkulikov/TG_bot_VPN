21c4777c7615e6d64ebc341431839ce331176711
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-23 23:23:24 +0300

    Added execute ssh command function

--- utils/generate_vpn_keys.py
+++ utils/generate_vpn_keys.py
@@ -1,11 +1,47 @@
+import socket
+from typing import Optional
+import paramiko
 from database.models import VPNKey, Server
 import os
 from config_data.config import BASE_DIR
+from loader import app_logger
 
 
 script_text = """
-
+echo 'Hello, world!'
 """
+
+
+
+def execute_ssh_command(
+    ip: str,
+    username: str,
+    password: str,
+    command: str,
+    timeout: int = 120
+) -> Optional[str]:
+    try:
+        with paramiko.SSHClient() as cl:
+            cl.load_system_host_keys()
+            cl.set_missing_host_key_policy(paramiko.RejectPolicy())
+            cl.connect(
+                hostname=ip,
+                username=username,
+                password=password,
+                look_for_keys=False,
+                allow_agent=False,
+                timeout=10
+            )
+            stdin, stdout, stderr = cl.exec_command(command, timeout=timeout)
+            output = stdout.read().decode('utf-8')
+            error = stderr.read().decode('utf-8')
+            if error:
+                app_logger.error(f"Error executing command: {error}")
+            return output
+    except (paramiko.AuthenticationException, paramiko.SSHException, socket.error, Exception) as e:
+        app_logger.error(f"SSH command execution failed: {e}")
+        return None
+
 
 def generate_key(server_obj: Server) -> VPNKey:
     """
