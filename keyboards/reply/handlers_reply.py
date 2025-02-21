05ac8304fcc49b9070a6f1f137d21a726a4062dc
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-02-21 21:51:06 +0300

    Update interface to reply buttons

--- keyboards/reply/handlers_reply.py
+++ keyboards/reply/handlers_reply.py
@@ -4,6 +4,6 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 def handlers_reply() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardMarkup(True, False, row_width=2,
                                    input_field_placeholder="Нажмите на нужную кнопку либо введите команду...")
-    keyboard.add(KeyboardButton('Серверы'), KeyboardButton('Справка'))
-    keyboard.add(KeyboardButton('Инструкция'))
+    keyboard.add(KeyboardButton('🌍 Серверы'), KeyboardButton('❓ Справка'))
+    keyboard.add(KeyboardButton('📖 Инструкция'))
     return keyboard
